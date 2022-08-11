7b8f1684b064ebdbdada9ce5c2c920f77ac85d31
Lokathor <zefria@gmail.com>
2022-08-11 02:25:15 +0000

    Deploying to gh-pages from @ Lokathor/kpasim@9efd5e55a38179751a3ac900d6b6a0dd432cbb5b 🚀

--- implementors/kpasim/data_bus/trait.DataBus.js
+++ implementors/kpasim/data_bus/trait.DataBus.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["kpasim"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"kpasim":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
